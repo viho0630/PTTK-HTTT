0b13d34ab2065034743680d61bb71356414a77bf
LeVanSon02 <levanson065@gmail.com>
2021-06-10 17:31:40 +0700

    test

---
+++ QLQuanCafe/QLQuanCafe/User_Controls/UC_DoiMatKhau.Designer.cs
@@ -0,0 +1,46 @@
+﻿
+namespace QLQuanCafe.User_Controls
+{
+    partial class UC_DoiMatKhau
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UC_DoiMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UC_DoiMatKhau";
+            this.Size = new System.Drawing.Size(416, 320);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
